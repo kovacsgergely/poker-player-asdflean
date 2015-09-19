8bea7c42679e1473885a9a186c38d522f4c73094
KovacsG <asdf@qwert.hu>
2015-09-19 10:23:34 +0200

    x

--- player.php
+++ player.php
@@ -7,14 +7,16 @@ class Player
     public function betRequest($game_state)
     {
     	$holeCards = array();
+    	$stack = 1000;
     	foreach ($game_state['players'] as $player) {
     		if (isset($player['hole_cards']) && !empty($player['hole_cards'])) {
     			$holeCards = $player['hole_cards'];
+    			$stack = $player['stack'];
     		}
     	}
     	foreach ($holeCards as $card) {
     		if (!in_array($card['rank'], array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))) {
-    			return 1000;
+    			return $stack;
     		}
     	}
     	
