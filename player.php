15734cfd61a4a32c585c9322a32be176ded48994
Kovács Gergely <kovacsgergely@Kovacs-MacBook-Air.local>
2015-09-19 10:17:27 +0200

    x

--- player.php
+++ player.php
@@ -6,7 +6,19 @@ class Player
 
     public function betRequest($game_state)
     {
-        return 1000;
+    	$holeCards = array();
+    	foreach ($game_state['players'] as $player) {
+    		if (isset($player['hole_cards']) && !empty($player['hole_cards'])) {
+    			$holeCards = $player['hole_cards'];
+    		}
+    	}
+    	foreach ($holeCards as $card) {
+    		if (!in_array($card['rank'], array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))) {
+    			return 1000;
+    		}
+    	}
+    	
+        return 0;
     }
 
     public function showdown($game_state)
